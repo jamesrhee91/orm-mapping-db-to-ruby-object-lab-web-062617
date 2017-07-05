89d1f4ac9d446fcd593ec46e36052442f89e290b
jamesrhee91 <jamesshrhee@gmail.com>
2017-07-05 19:05:01 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -3,27 +3,84 @@ class Student
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
+    student = self.new
+    student.id = row[0]
+    student.name = row[1]
+    student.grade = row[2]
+    student
   end
 
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
+    sql = <<-SQL
+      SELECT * FROM students
+    SQL
+
+    rows = DB[:conn].execute(sql)
+    rows.map {|row| self.new_from_db(row)}
   end
 
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
+    sql = <<-SQL
+      SELECT * FROM students WHERE name = ?
+    SQL
+
+    row = DB[:conn].execute(sql, name)[0]
+    self.new_from_db(row)
   end
-  
+
+  def self.count_all_students_in_grade_9
+    sql = <<-SQL
+      SELECT * FROM students WHERE grade = ?
+    SQL
+
+    DB[:conn].execute(sql, 9)
+  end
+
+  def self.students_below_12th_grade
+    sql = <<-SQL
+      SELECT * FROM students WHERE grade < ?
+    SQL
+
+    DB[:conn].execute(sql, 12)
+  end
+
+  def self.first_x_students_in_grade_10(n)
+    sql = <<-SQL
+      SELECT * FROM students WHERE grade = ?
+    SQL
+
+    DB[:conn].execute(sql, 10).take(n)
+  end
+
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+      SELECT * FROM students WHERE grade = ? LIMIT 1
+    SQL
+
+    self.new_from_db(DB[:conn].execute(sql, 10)[0])
+  end
+
+  def self.all_students_in_grade_x(n)
+    sql = <<-SQL
+      SELECT * FROM students WHERE grade = ?
+    SQL
+
+    DB[:conn].execute(sql, n)
+  end
+
   def save
     sql = <<-SQL
-      INSERT INTO students (name, grade) 
+      INSERT INTO students (name, grade)
       VALUES (?, ?)
     SQL
 
     DB[:conn].execute(sql, self.name, self.grade)
   end
-  
+
   def self.create_table
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
